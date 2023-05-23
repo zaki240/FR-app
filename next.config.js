ebc5576614fb7304c136b5011f224ae46568ff41
ahmadfahrurrozi24 <afahrurrozy07@gmail.com>
2023-05-23 08:07:49 +0700

    home

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'jealous-garage.pockethost.io',
+        port: '',
+        pathname: '/api/files/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- pages/home.js
+++ pages/home.js
@@ -1,40 +1,58 @@
 import React from 'react'
 import Image from 'next/image'
+import pb from "@/lib/pocketbase";
 
-export default home
-function home() {
+export const getStaticProps = async () => {
+  const resultList = await fetch('https://jealous-garage.pockethost.io/api/collections/all_restaurant/records')
+  const res = await resultList.json()
+  return { props: { res } };
+};
+
+export default function home({ res }) {
+
+  res.items.map((value) => {
+    console.log(value)
+  })
+
   return (
     <>
-    {/* Home title */}
-    <div className="title text-center text-3xl text-black font-bold">
-      <h1 >Restaurant In Indonesia</h1>
-    </div>
-
-    {/* Kolom */}
-    <div className="kolom-title   p-3  font-bold text-black text-xl">
-      <h1>Rekomendasi Populer</h1>
-<div className="kolom-gambar p-4">
-  <div className="konten-gambar ml-72 flex p-4 gap-6">
-    <div className="gambar   font-bold text-black">
-      <h1 className='isiGambar text-4xl'>GAMBAR 1</h1>
-      <div className="rating">
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+      {/* Home title */}
+      <div className="title text-center text-3xl text-black font-bold">
+        <h1 >Restaurant In Indonesia</h1>
       </div>
-    </div>
-    
-  <h1>GAMBAR 2</h1>
-  <h1>GAMBAR 3</h1>
-  </div>
-    <div className="konten-gambar ml-72 flex p-4 gap-6 text-4xl font-bold text-black">
-    <h1 className='isiGambar text-4xl'>GAMBAR 4</h1>
-    <h1 className='isiGambar text-4xl'>GAMBAR 5</h1>
-    <h1 className='isiGambar text-4xl'>GAMBAR 6</h1>
-  </div>
-</div>
-    </div>
+
+      {/* Kolom */}
+      <div className="kolom-title   p-3  font-bold text-black text-xl">
+        <h1>Rekomendasi Populer</h1>
+        <div className="kolom-gambar p-4">
+          <div className="konten-gambar ml-72 flex p-4 gap-6">
+            <div className="gambar   font-bold text-black">
+              <h1>{res.page}</h1>
+              <div className='grid grid-cols-3 gap-3'>
+              {res.items.map((value) => {
+                return (
+                  <div>
+                    <h1 className='isiGambar text-4xl'>{value['nama']}</h1>
+                        <Image 
+                        src={`https://jealous-garage.pockethost.io/api/files/${value["collectionId"]}/${value["id"]}/${value["image"][0]}`}
+                        height={200}
+                        width={200}
+                        />
+
+                    <div className="rating">
+                      <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                      <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                      <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                      <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+              </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
