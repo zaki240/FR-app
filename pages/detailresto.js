9eeb5dfcde5504d8c74eb2e3bffcba560a70e19c
zaki240 <mzakiputra24@gmail.com>
2023-05-24 21:20:57 +0700

    card

--- pages/detailresto.js
+++ pages/detailresto.js
@@ -1,50 +1,44 @@
-import React from 'react'
-import { FiMapPin } from 'react-icons/fi';
-import { BiTime } from 'react-icons/bi';
-import { FaPhoneSquareAlt } from 'react-icons/fa';
-import {MdOutlineAttachMoney} from 'react-icons/md';
+import React from "react";
+import { FiMapPin } from "react-icons/fi";
+import { BiTime } from "react-icons/bi";
+import { BsTelephone } from "react-icons/bs";
+import { MdOutlineAttachMoney } from "react-icons/md";
 
 export default function detailresto() {
   return (
     <>
-    <div className="konten-detail-resto ml-96 flex gap-10">
-      <div className="detail-resto-gambar text-3xl text-black font-bold">
-      Gambar
+      <div className="konten-detail-resto ml-96 flex gap-10">
+        <div className="detail-resto-gambar text-3xl text-black font-bold">
+          Gambar
         </div>
         <div className="deskripsi-resto text-black py-3">
           <div className="nama-resto flex text-3xl text-black border-b-2 border-black justify-between">
-          <h1>
-          Restauran agung
-          </h1>
-             <div>
-              3.4
-             </div>
+            <h1>Restauran agung</h1>
+            <div>3.4</div>
           </div>
-          <div className="container">
+          <div className="container"></div>
+          <div className="flex p-2 items-center gap-1">
+            <FiMapPin />
+            <h1>alamat: Amerika tenggara jalan mexico deket sungai gangga</h1>
+          </div>
+          <div className="flex p-2 items-center gap-1">
+            <MdOutlineAttachMoney />
+            <h1>Di baawah $400,000/orang</h1>
+          </div>
+          <div className="flex p-2 items-center gap-1">
+            <BiTime />
+            <h1>Jam Buka: setiap hari kecuali hari Kiamat</h1>
+          </div>
+          <div className="flex p-2 items-center gap-1">
+            <BsTelephone />
+            <h1>No Telp: 088008184028930 </h1>
+          </div>
 
-          </div>
-          <div className="flex p-2 items-center gap-1">
-            <FiMapPin/>
-          <h1>alamat: Amerika tenggara jalan mexico deket sungai gangga</h1>
-          </div>
-          <div className="flex p-2 items-center gap-1">
-            <MdOutlineAttachMoney/>
-          <h1>Di baawah $400,000/orang</h1>
-          </div>
-          <div className="flex p-2 items-center gap-1">
-            <BiTime/>
-          <h1>Jam Buka: setiap hari kecuali hari Kiamat</h1>
-          </div>
-          <div className="flex p-2 items-center gap-1">
-            <FaPhoneSquareAlt/>
-          <h1>No Telp: 088008184028930 </h1>
-          </div>
-          
-            <button className="btn btn-wide text-white mt-12">Tulis Review</button>
-
-          </div>  
-        
-    </div>
+          <button className="btn btn-wide text-white mt-12">
+            Tulis Review
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,81 @@
-import Head from 'next/head'
-import { useState } from "react";
+import React from "react";
+import Image from "next/image";
+import pb from "@/lib/pocketbase";
+import Link from "next/link";
 
+export const getStaticProps = async () => {
+  const resultList = await fetch(
+    "https://jealous-garage.pockethost.io/api/collections/all_restaurant/records"
+  );
+  const res = await resultList.json();
+  return { props: { res } };
+};
 
-export default function Home() {
-  
+export default function home({ res }) {
+  res.items.map((value) => {
+    console.log(value);
+  });
+
   return (
     <>
-    <div>
-      
-    </div>
+      {/* Home title */}
+      <div className="title text-center text-3xl text-black font-bold mt-5  ">
+        <h1>Restaurant In Indonesia</h1>
+      </div>
+      {/* Kolom */}
+      <div className="kolom-title   p-3  font-bold text-black text-xl">
+        <h1>Rekomendasi Populer</h1>
+        <div className="kolom-gambar p-4">
+          <div className="konten-gambar ml-72 flex p-4 gap-6">
+            <div className="gambar   font-bold text-black">
+              {/* <h1>{res.page}</h1> */}
+              <div className="grid grid-cols-3 gap-15">
+                {res.items.map((value) => {
+                  return (
+                    <a
+                      href="/detailresto"
+                      className="p-3 border-solid border-2 rounded-md border-gray-300 shadow m-3 hover:scale-105"
+                    >
+                      <h1 className="isiGambar text-bold">{value["nama"]}</h1>
+                      <Image
+                        className="p-1"
+                        src={`https://jealous-garage.pockethost.io/api/files/${value["collectionId"]}/${value["id"]}/${value["image"][0]}`}
+                        height={200}
+                        width={200}
+                      />
+
+                      <div className="rating">
+                        <input
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 bg-orange-400"
+                        />
+                        <input
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 bg-orange-400"
+                        />
+                        <input
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 bg-orange-400"
+                          checked
+                        />
+                        <input
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 bg-orange-400"
+                        />
+                      </div>
+                      {/* <h1 className='isiGambar text-4xl'>{value['alamat']}</h1> */}
+                    </a>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
